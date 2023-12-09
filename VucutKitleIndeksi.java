c478eb40cbd61ad95c00e126cc79e4e3f10e7420
Bora Tekin <pikacu1903@gmail.com>
2023-12-09 14:22:28 +0300

    Create VucutKitleIndeksi.java

---
+++ VucutKitleIndeksi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        double boy;
+        int kilo;
+
+
+        System.out.println("Kilonuzu Giriniz  : ");
+        kilo = scanner.nextInt();
+        System.out.println("Boyunuzu Giriniz (metre cinsinden) : ");
+        boy = scanner.nextDouble();
+
+        double KitleIndeksi = kilo / (boy * boy);
+        
+        System.out.println("Vücut Kitle Indeksiniz : " + KitleIndeksi);
+
+    }
+}
